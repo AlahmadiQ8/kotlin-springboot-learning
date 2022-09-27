6fe100a8502256e00a9fdeefad0973ab8352e599
Mohammad Mohammad <mmohammadq8@outlook.com>
2022-09-27 20:50:38 +0300

    more changes

--- src/main/kotlin/net/momo/kotlinsprintboot/domain/Project.kt
+++ src/main/kotlin/net/momo/kotlinsprintboot/domain/Project.kt
@@ -3,22 +3,22 @@ package net.momo.kotlinsprintboot.domain
 import java.time.LocalDate
 import javax.persistence.*
 
+
 @Table(name = "projects", indexes = [
     Index(name = "idx_project_name", columnList = "name")
 ])
 @Entity
 class Project(
-    @Column(name = "name")
-    var name: String,
+    @Column(name = "name", nullable = false)
+    var name: String? = "",
 
     @Column(name = "date_created")
-    var dateCreated: LocalDate = LocalDate.now(),
+    var dateCreated: LocalDate? = null,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: String? = null) {
+    var id: String? = null,
 
-    constructor() : this("") {
-
-    }
-}
+    @OneToMany(mappedBy = "project", fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
+    open var tasks: MutableList<Task> = mutableListOf()
+)
---
+++ src/main/kotlin/net/momo/kotlinsprintboot/domain/Task.kt
@@ -0,0 +1,36 @@
+package net.momo.kotlinsprintboot.domain
+
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+@Table(name = "task")
+open class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    open var id: Long? = null
+
+    @Column(name = "name", nullable = false)
+    open var name: String = ""
+
+    @Column(name = "description")
+    open var description: String? = null
+
+
+    @Column(name = "date_created", nullable = false)
+    open var dateCreated: LocalDate? = null
+
+    @Column(name = "due_date", nullable = false)
+    open var dueDate: LocalDate? = null
+
+    @Enumerated
+    @Column(name = "task_status", nullable = false)
+    open var taskStatus: TaskStatus? = null
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    open var project: Project? = null
+}
+
+enum class TaskStatus { NEW, IN_PROGRESS, COMPLETE }
